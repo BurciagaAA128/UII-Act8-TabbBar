1fb3b077b74ab0d0f541aa194103eec36dc7b446
Aaron Burciaga <146780951+BurciagaAA128@users.noreply.github.com>
2024-04-26 08:57:02 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,117 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar Aaron Burciaga",
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const MiPaginaInicial(),
+    );
+  }
+} //Fin AppMiTabBar
+
+//Stateful
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
+
+  @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} //MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tabbar Pockets"),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "Platillos",
+                icon: Icon(Icons.lunch_dining),
+              ),
+              Tab(
+                text: "Bebidas",
+                icon: Icon(Icons.no_drinks),
+              ),
+              Tab(
+                text: "Gerente",
+                icon: Icon(Icons.badge),
+              ),
+              Tab(
+                text: "Clientes",
+                icon: Icon(Icons.people),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: const <Widget>[
+            _StyledImage(
+              imageUrl: "assets/burguir.jpg",
+            ),
+            _StyledImage(
+              imageUrl:
+                  "https://github.com/BurciagaAA128/Img_IOS/blob/main/cocacola.jpg?raw=true",
+            ),
+            _StyledImage(
+              imageUrl: "assets/gerent.jpg",
+            ),
+            _StyledImage(
+              imageUrl:
+                  "https://github.com/BurciagaAA128/Img_IOS/blob/main/persona2.jpg?raw=true",
+            ),
+          ],
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class _StyledImage extends StatelessWidget {
+  final String imageUrl;
+  const _StyledImage({required this.imageUrl, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 500,
+            height: 400,
+            margin: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image(
+                image: imageUrl.startsWith('http')
+                    ? NetworkImage(imageUrl)
+                    : AssetImage(imageUrl) as ImageProvider,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
